48935f8d58061922f78efcf282d98ae5a2d504c4
Blevene <blevene@salesforce.com>
2013-08-30 12:12:20 -0400

    Create ccparser.py

---
+++ ccparser.py
@@ -0,0 +1,27 @@
+# Read Comment Crew daily report and parse out URLs
+from sys import argv
+from datetime import datetime
+
+# Define a method to replace multiple strings in the matching text 
+def replace_things(text, dic):
+	for original, altered in dic.iteritems():
+		text = text.replace(original,altered)
+	return text
+
+# Dictionary of strings to replace
+rep = {'hxxp':'http', '[.]':'.'}
+# Take the first arg from command line as the file to parse
+daily_report = argv[1]
+# Open the file
+ccreport = open(daily_report, 'rb') 
+# Print the date + time when the script is run
+print datetime.now()
+# Iterate over the file, looking for lines that start with URL
+# For lines that start with URL, replace the text as defined
+# by the dict named rep
+for line in ccreport:
+	if line.startswith('URL'):
+		replaced = replace_things(line, rep)
+ # This removes the "URL: " and trailing colon + newline		
+		replaced = replaced[5:-4]
+		print replaced
