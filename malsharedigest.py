25f9b1f18ca2a6f54c53777ab2fc0d7a476755be
Blevene <blevene@salesforce.com>
2013-08-31 16:49:26 -0400

    Update malsharedigest.py
    Added a validity check to -s to ensure the value passed is a md5 hash

--- malsharedigest.py
+++ malsharedigest.py
@@ -9,38 +9,39 @@ import argparse
 import csv
 from sys import argv
 from datetime import datetime, date
-# Add options -s and -p
+import re
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--search",
 	help="Search the consolidated results file for a hash")
 parser.add_argument("-p", "--pull", action="store_true",
 	help="Pull the most recent Malshare digest")
-# Check if an argument is supplied
 if len(argv) < 2:
 	parser.print_help()
 args = parser.parse_args()
-# Set a results flag, default false (this is only for -s)
 found = False
 if (args.pull):
 	mal_digest = urllib2.urlopen(url='http://www.malshare.com/daily/malshare.current.txt')
 	mal_digest = list(mal_digest)
-# Make a list containing the pull date that is the same length as mal_digest
-	pull_time = [str(date.today())] * len(mal_digest)
-# Strip out the /n that gets appended to each item in the list	
+	pull_time = [str(date.today())] * len(mal_digest) 
 	strip_list = [x.strip('\n') for x in mal_digest]
-# Create a dictionary out of strip_list and pull_time [key, value]	
 	dictionary = dict(zip(strip_list, pull_time))
 	writer = csv.writer(open('dictresults.csv', 'a'))
 	for key, value in dictionary.items():
 		writer.writerow([key, value])
-		writer.close()
 
 elif (args.search):
-	with open('dictresults.csv', 'r') as f:
-		reader = csv.reader(f)
-		for num, row in enumerate(reader):
-			if args.search in row[0]:
-				print "%s was seen on %s" % (row[0], row[1])
-				found = True
-		if found != True:
-			print "Sorry, we don't have that hash."
+	# Check to see if the param passed is a valid hash
+	validity_check = re.compile(r'(?=(\b[A-Fa-f0-9]{32}\b))')
+	if validity_check.match(args.search) == None:
+		print "That isn't a md5 sum."
+		exit()
+	elif validity_check.match(args.search):
+		with open('dictresults.csv', 'r') as f:
+			reader = csv.reader(f)
+			for num, row in enumerate(reader):
+				if args.search in row[0]:
+					print "%s was seen on %s" % (row[0], row[1])
+					found = True
+	if found != True:
+		print "Sorry, we don't have that hash."
