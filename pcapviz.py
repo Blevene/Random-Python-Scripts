9f513dfe15b8dcc6578bfb5986520ea58e91298d
Blevene <blevene@salesforce.com>
2013-11-18 19:34:35 -0500

    Removed "implied" in -a, now if no args are given, an error is thrown.

--- pcapviz.py
+++ pcapviz.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Visualize a .pcap file using scapy and afterglow.")
 parser.add_argument("-c", "--conversations", help="Use this to view a conversations summary.",
 	action="store_true")
-parser.add_argument("-a", "--afterglow", help="Use this to view a complete viz, this is implied.",
+parser.add_argument("-a", "--afterglow", help="Use this to view a complete viz.",
 	action="store_true")	
 args = parser.parse_args()
 
